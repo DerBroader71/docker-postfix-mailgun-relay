135532ac3ad9deda4eac750e536e338c529a9075
Ralf Herzog <ralf.herzog@mail.com>
2017-04-02 18:18:42 +0200

    Installed SASL module for postfix and LDAP support

--- init.sh
+++ init.sh
@@ -8,10 +8,10 @@ if [ ! -z "${SYSTEM_TIMEZONE}" ]; then
 fi
 
 # Set mynetworks for postfix relay
-if [ ! -z "${MYNETWORKS}" ]; then
-    echo "setting mynetworks = ${MYNETWORKS}"
-    postconf -e mynetworks="${MYNETWORKS}"
-fi
+#if [ ! -z "${MYNETWORKS}" ]; then
+#    echo "setting mynetworks = ${MYNETWORKS}"
+#    postconf -e mynetworks="${MYNETWORKS}"
+#fi
 
 # General the email/password hash and remove evidence.
 if [ ! -z "${EMAIL}" ] && [ ! -z "${EMAILPASS}" ]; then
@@ -24,3 +24,29 @@ else
 fi
 unset EMAIL
 unset EMAILPASS
+
+if [ ! -z "${DOMAIN}" ]; then
+    echo "set default domain"
+    postconf -e smtpd_sasl_local_domain = example.com
+fi
+
+if [ ! -z "${SASL_LDAP_SERVER}" ]; then
+    echo "setting sasl ldap settings"
+    cat << EOF > /etc/saslauthd.conf
+ldap_servers: ldap://${SASL_LDAP_SERVER}
+
+ldap_bind_dn: ${SASL_LDAP_BIND_DN}
+ldap_password: ${SASL_LDAP_BIND_DN_PASSWORD}
+
+ldap_search_base: ${SASL_LDAP_USERS}
+ldap_filter: ${SASL_LDAP_USERS_FILTER}
+EOF
+fi
+
+unset \
+ SASL_LDAP_SERVER \
+ SASL_LDAP_BIND_DN \
+ SASL_LDAP_BIND_DN_PASSWORD \
+ SASL_LDAP_USERS \
+ SASL_LDAP_USERS_FILTER \
+ SASL_LDAP_GENERATE_HOMEDIR
